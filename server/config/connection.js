8cfe049e7591d58e81ed4c59dda91a22d82ebffc
Vader <gassanb97@gmail.com>
2022-12-06 13:58:04 -0800

    debugging

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks', {
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
+  //Commented out code bellow in order for the application to work in Heroku
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 module.exports = mongoose.connection;
